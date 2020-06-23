9ef6b589bbd5702684a91c1f2e79ac1b47497620
lelloman <cerasuolodomenico@gmail.com>
2020-06-23 08:44:13 +0200

    Remove ilmattino paywall with observer

--- ilmattino.js
+++ ilmattino.js
@@ -1,7 +1,16 @@
 (function() {
-  var element = document.getElementById("paywall_wrapper");
-  if(element != undefined) {
-    element.parentNode.removeChild(element);
-    document.body.style = "";
-  }
+	const removePaywall = function() {
+		var element = document.getElementById("paywall_wrapper");
+		if(element != undefined) {
+		element.parentNode.removeChild(element);
+		document.body.style = "";
+		}
+  	}
+  	var observer = new MutationObserver(function(mutations) {
+	    removePaywall();
+	});
+	
+	var config = { attributes: false, childList: true, characterData: false }
+
+	observer.observe(document.body, config);
 })();
